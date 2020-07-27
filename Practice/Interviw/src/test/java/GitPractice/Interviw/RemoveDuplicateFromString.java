1eb3f5d53aca5418f5a4298a49c656f51f2f28b4
Dharmendra <contact4dj@gmail.com>
2020-07-27 16:41:24 -0500

    Remove Duplicate From String

---
+++ Practice/Interviw/src/test/java/GitPractice/Interviw/RemoveDuplicateFromString.java
@@ -0,0 +1,29 @@
+package GitPractice.Interviw;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class RemoveDuplicateFromString {
+
+	public static String removeDuplicate(String str) {
+	    char[] c = str.toCharArray();
+	    String newStr = "";
+	    for(int i=0; i<=c.length-1; i++){
+	        if(!newStr.contains(c[i]+ ""))
+	        newStr = newStr + c[i];
+	    }
+		return newStr;
+	}
+
+  @Test
+  public void negativeTest() {
+	  Assert.assertNotEquals("regression", removeDuplicate("regression"));
+	  System.out.println("Negative Test Passed");
+  }
+  @Test
+  public void posativeTest() {
+	  Assert.assertEquals("regsion", removeDuplicate("regression"));
+	  System.out.println("Posative Test Passed");
+  }
+}

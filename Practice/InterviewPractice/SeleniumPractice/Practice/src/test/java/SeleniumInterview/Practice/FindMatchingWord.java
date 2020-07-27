196b1f29b210c8625000f3ab01fa2a416d08e1ca
Dharmendra <contact4dj@gmail.com>
2020-07-27 15:09:07 -0500

    Matching Word TestNg

--- Practice/InterviewPractice/SeleniumPractice/Practice/src/test/java/SeleniumInterview/Practice/FindMatchingWord.java
+++ Practice/InterviewPractice/SeleniumPractice/Practice/src/test/java/SeleniumInterview/Practice/FindMatchingWord.java
@@ -1,16 +1,27 @@
 package SeleniumInterview.Practice;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class FindMatchingWord {
-	public static void main(String[] args) {
-		String str = "Hello World";
-		String []searchArray = {"hello", "world","sun","moon"}; 
-		for(String s: searchArray) {
-			if(str.toLowerCase().contains(s)) {
-				System.out.println(s+":True");
-			}
-			else {
-				System.out.println(s+":False");
+		public static void MatchingString() {
+			String str = "Hello World";
+			String []searchArray = {"hello", "world","sun","moon"}; 
+			for(String s: searchArray) {
+				if(str.toLowerCase().contains(s)) {
+					System.out.println(s+":True");
+				}
+				else {
+					System.out.println(s+":False");
+				}
 			}
 		}
-	}
+	  @Test
+	  public void posativeTest() {
+		  Assert.assertTrue(("hello world").contains("hello"));
+	  }
+	  @Test
+	  public void negativeTest() {
+		  Assert.assertFalse(("hello world").contains("sun"));
+	  }
 }
